946b2fc684380ec743a9a6e35bd046ba755c113b
Ritesh5444 <riteshkvt.jsr@gmail.com>
2023-06-20 01:06:23 +0530

    update template.py file

--- template.py
+++ template.py
@@ -1,15 +1,16 @@
 import os
-import pathlib import Path
-import logging 
+from pathlib import Path
+import logging
 
-logging. basicConfig(level=logging.INFO, format= '[%(asctime)s]: %(message)s:')
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
 
 project_name = "signLanguage"
 
-# ".github/workflows/.gitkeep", -used to have CICD pipeline
+
 list_of_files = [
     ".github/workflows/.gitkeep",
-    "data/.gitkeep", #created so that empty folder is not created in github
+    "data/.gitkeep",
     "docs/.gitkeep",
     f"{project_name}/__init__.py",
     f"{project_name}/components/__init__.py",
@@ -17,64 +18,47 @@ list_of_files = [
     f"{project_name}/components/data_validation.py",
     f"{project_name}/components/model_trainer.py",
     f"{project_name}/components/model_pusher.py",
-
     f"{project_name}/configuration/__init__.py",
     f"{project_name}/configuration/s3_operations.py",
-    f"{project_name}/configuration/s3_operations.py",
-
     f"{project_name}/constant/__init__.py",
     f"{project_name}/constant/training_pipeline/__init__.py",
-    f"{project_name}/constant/appication.py",
-
+    f"{project_name}/constant/application.py",
     f"{project_name}/entity/__init__.py",
     f"{project_name}/entity/artifacts_entity.py",
     f"{project_name}/entity/config_entity.py",
-
     f"{project_name}/exception/__init__.py",
     f"{project_name}/logger/__init__.py",
-
     f"{project_name}/pipeline/__init__.py",
-    f"{project_name}/pipeline/traning_pipeline.py",
-
+    f"{project_name}/pipeline/training_pipeline.py",
     f"{project_name}/utils/__init__.py",
     f"{project_name}/utils/main_utils.py",
-
     "template/index.html",
     ".dockerignore",
     "app.py",
     "Dockerfile",
     "requirements.txt",
     "setup.py"
+
+
 ]
+
 
 for filepath in list_of_files:
     filepath = Path(filepath)
 
-    filedir, filename = os.path.split
+    filedir, filename = os.path.split(filepath)
 
-if filedir!= "":
-    os.makedirs(filedir, exist_ok= True)
-    logging.info(f"Creating directory: {filedir} for the file {filename}")
+    if filedir !="":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory: {filedir} for the file {filename}")
 
-if (not os.path.exists(filename)) or (os.path.getsize(filename)== 0):
-    with open(filepath,'w') as f:
-        pass
-        logging.info(f"Creating empty file: {filename}")
+    
+    if(not os.path.exists(filename)) or (os.path.getsize(filename) == 0):
+        with open(filepath, 'w') as f:
+            pass
+            logging.info(f"Creating empty file: {filename}")
 
-else:
-    logging.info(f"{filename} is already created")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
+    else:
+        logging.info(f"{filename} is already created")
 
